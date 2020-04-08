2203eb5c5a9870256648187fb48b744fb6fc2be9
alkaitagi <alkaitagi@outlook.com>
2020-04-08 11:17:53 +0300

    Styling

--- web-editor/src/App.vue
+++ web-editor/src/App.vue
@@ -19,7 +19,7 @@
       @duplicate="slide.objects.push(slide.objects[i])"
       v-for="(obj, i) in slide.objects"
     />-->
-    <div class="card" style="background-color: #D8DEE9"></div>
+    <div class="card" style="background-color: var(--select)"></div>
   </div>
 </template>
 
