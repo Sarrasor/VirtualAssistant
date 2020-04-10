85d2926cddbc01aaacf7ea40c01c422d27b60661
Sarrasor <danikdanik481@gmail.com>
2020-04-10 22:34:48 +0300

    add capture image button

--- virtual-assistant/app/src/main/java/com/example/virtual_assistant/InstructionDisplay.java
+++ virtual-assistant/app/src/main/java/com/example/virtual_assistant/InstructionDisplay.java
@@ -1,29 +1,40 @@
 package com.example.virtual_assistant;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
@@ -32,6 +43,7 @@ import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -39,6 +51,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -66,7 +80,7 @@ public class InstructionDisplay extends AppCompatActivity {
     private Context context;
     private int step_number;
     private List<Step> steps;
-    private ArFragment arFragment;
+    private WritingArFragment arFragment;
     private ImageView stepImgView;
     private TextView stepTxtView;
     private ViewRenderable imgRenderable;
@@ -201,6 +215,7 @@ public class InstructionDisplay extends AppCompatActivity {
      */
     public void nextStep(View view)
     {
+
         // Set new step number
         if (step_number < steps.size() - 1) {
             step_number += 1;
@@ -301,7 +316,6 @@ public class InstructionDisplay extends AppCompatActivity {
     }
 
 
-
     /**
      * Displays the first step of the instruction of plane tap
      * @param instruction_dir path to the instruction
@@ -309,7 +323,7 @@ public class InstructionDisplay extends AppCompatActivity {
     private void displayInstruction(File instruction_dir)
     {
         // Create AR fragment
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        arFragment = (WritingArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
         // Get current step
         Step step = steps.get(step_number);
@@ -422,5 +436,87 @@ public class InstructionDisplay extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public void takePhoto(View view)
+    {
+        final String filename = generateFilename();
+
+        ArSceneView ar_view = arFragment.getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(ar_view.getWidth(), ar_view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(ar_view, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS)
+            {
+                try
+                {
+                    saveBitmapToDisk(bitmap, filename);
+                }
+                catch (IOException e)
+                {
+                    Toast toast = Toast.makeText(InstructionDisplay.this, e.toString(),
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+
+                Snackbar snackbar = Snackbar.make(view, "Photo saved", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Open in Photos", v ->
+                {
+                    File photoFile = new File(filename);
+
+                    Uri photoURI = FileProvider.getUriForFile(InstructionDisplay.this,
+                            InstructionDisplay.this.getPackageName() + ".ar.codelab.name.provider",
+                            photoFile);
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, photoURI);
+                    intent.setDataAndType(photoURI, "image/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+
+                });
+                snackbar.show();
+            }
+            else {
+                Toast toast = Toast.makeText(InstructionDisplay.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+    private String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator + "Sceneform/" + date + "_screenshot.jpg";
+    }
+
+    private void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+        File out = new File(filename);
+        if (!out.getParentFile().exists())
+        {
+            out.getParentFile().mkdirs();
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(filename);
+             ByteArrayOutputStream outputData = new ByteArrayOutputStream())
+        {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+        }
+        catch (IOException ex)
+        {
+            throw new IOException("Failed to save bitmap to disk", ex);
+        }
     }
 }
