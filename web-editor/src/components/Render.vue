b773d07bf9c88543952378c73298993d0b8068ab
alkaitagi <alkaitagi@outlook.com>
2020-05-02 23:45:50 +0300

    Fix

--- web-editor/src/components/Render.vue
+++ web-editor/src/components/Render.vue
@@ -35,13 +35,9 @@ export default {
   },
   methods: {
     render() {
-      if (this.assets) {
-        let ids = this.assets.map(a => a.id);
-        this.ids
-          .filter(i => !ids.includes(i))
-          .forEach(i => scene.deleteAsset(i));
-        this.ids = ids;
-      }
+      let ids = (this.assets ?? []).map(a => a.id);
+      this.ids.filter(i => !ids.includes(i)).forEach(i => scene.deleteAsset(i));
+      this.ids = ids;
 
       scene.init(this.slide, this.assets, this.files);
     }
