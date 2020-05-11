6cbd61744cc6459c107df0532d14cfcc29f98f22
Daniil Burakov <danikdanik481@gmail.com>
2020-05-11 13:49:48 +0300

    Update vue.config.js

--- web-editor/vue.config.js
+++ web-editor/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/home/sarrasor/Documents/Projects/VirtualAssistant/INNO-S20-SP/web-editor/dist'
+        ? '/INNO-S20-SP'
         : '/'
 }
